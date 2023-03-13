ba5e53e074d24a54182d173b4b3ad1a39028d061
AlBlinder <127503733+AlBlinder@users.noreply.github.com>
2023-03-13 14:08:08 +0100

    Prova (gravetat)
    Això només és una prova (canviada la direcció de la gravetat).

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 var velocitat := 500
 var moviment := Vector2.ZERO
 var gravetat := Vector2.UP * 3000
-var salt := Vector2.DOWN * 70
+var salt := Vector2.UP * 70
 var dreta := Vector2.RIGHT * 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,7 +15,7 @@ func _ready():
 func _process(delta):
 	moviment += gravetat * delta
 #	moviment.x = Input.get_axis("ves_esquerra", "ves_dreta") * velocitat
-	if Input.is_action_pressed("ves_baix"):
+	if Input.is_action_pressed("ves_dalt"):
 		moviment += salt
 		
 	moviment = move_and_slide(moviment + dreta * delta, Vector2(0,-1))
