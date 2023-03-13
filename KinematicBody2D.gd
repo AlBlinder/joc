607ad7a2df1366750c3c79112b3be3b5c658c5fb
AlBlinder <127503733+AlBlinder@users.noreply.github.com>
2023-03-13 14:27:12 +0100

    Contador combustible
    Avançada la LABEL del personatge principal amb un contador del combustible per a que caigui quan és zero.

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,9 +3,11 @@ extends KinematicBody2D
 
 var velocitat := 500
 var moviment := Vector2.ZERO
-var gravetat := Vector2.UP * 3000
+var gravetat := Vector2.DOWN * 3000
 var salt := Vector2.UP * 70
 var dreta := Vector2.RIGHT * 100
+var combustible := 1000
+var contador := 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	position = Vector2(500,300)
@@ -13,10 +15,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	moviment += gravetat * delta
 #	moviment.x = Input.get_axis("ves_esquerra", "ves_dreta") * velocitat
-	if Input.is_action_pressed("ves_dalt"):
-		moviment += salt
-		
+	if combustible > 0:
+		$Label.text = str(combustible/10)
+		contador = 0
+		if Input.is_action_pressed("ves_dalt"):
+			moviment += salt 
+			combustible -= 1
+	elif combustible < 0:
+		$Label.text = "espera " + str(contador)
+		contador += 1
 	moviment = move_and_slide(moviment + dreta * delta, Vector2(0,-1))
 #	pass
