5d25d723fe0902d0c348ecbd317ff0ff651a815a
AlBlinder <127503733+AlBlinder@users.noreply.github.com>
2023-03-19 13:24:34 +0100

    Meta
    Feta la meta amb un text per concloure i els crèdits.

--- Node2D.gd
+++ Node2D.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var vides = 5
+var meta = 0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,7 +12,7 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-
+	if meta == 1: $Personatge.position = Vector2(33300, 0)
 	if vides <= 0:
 		$Personatge.position = Vector2(0,0)
 		vides = 5
@@ -32,3 +33,8 @@ func _on_Area2D_body_entered(body):
 func _on_Area2D_foc_body_entered(body):
 	if body.name == "Personatge":
 		vides = 0
+
+
+func _on_Area2D_body_entered_meta(body):
+	meta = 1
+	pass # Replace with function body.
